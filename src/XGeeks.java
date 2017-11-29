8758c87586468c54bc25fcff0ab424a0a30451fb
tippur.sanchitha <tippur.sanchitha@SEMS-259-13.SEMS259.LAB>
2017-11-29 14:41:58 -0800

    XGeeks.class- bin

--- src/XGeeks.java
+++ src/XGeeks.java
@@ -1,8 +1,6 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
-
-
 
 import javax.swing.JOptionPane;
 
@@ -14,12 +12,37 @@ import javax.swing.JOptionPane;
 public class XGeeks {
 public static void main(String[] args) {
 
-		// 1. Save the superpower for each person in a variable.
+	// 1. Save the superpower for each person in a variable.
+	String SanchithaPower = "mind-reading";
+	String JacobpPower = "making people salty";
+	String JacobjPower = "teleportation";
+	String KellerPower = "Telekinesis";
+	String DerekPower = "making people salty";
 
-		// 2. Ask the user to enter a name. Store their answer in a variable.
-
-		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
+	// 2. Ask the user to enter a name. Store their answer in a variable.
+String name=JOptionPane.showInputDialog("enter your name");
+	// 3. Show the superpower in a pop-up, depending on the name entered.
+	if(name.equals("Sanchitha")){
+		System.out.println("Sanchithas power is " + SanchithaPower);
+		JOptionPane.showMessageDialog(null, "SanchithaPower is mind reading");
 	}
+	if(name.equals("JabobpPower")){
+		System.out.println("Jacobp power is " + JacobpPower);
+		JOptionPane.showMessageDialog(null, "JacobpPower is making people salty");
+	}
+	if(name.equals("Derek")){
+		System.out.println("Derek power is " + DerekPower);
+		JOptionPane.showMessageDialog(null, "DerekPower is making people salty");
+	}
+	if(name.equals("Keller")){
+		System.out.println("Keller power is " + KellerPower);
+		JOptionPane.showMessageDialog(null, "KellerPower is Telekinesis");
+		
+		}
+	if(name.equals("JacobjPower")){
+		System.out.println("Jacobj power is "+JacobjPower);
+		JOptionPane.showMessageDialog(null, "JacobjPower is teleportation");
+		
+			}
+		}
 }
-
